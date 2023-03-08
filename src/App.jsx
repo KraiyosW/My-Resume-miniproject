01950c5a66a61a634e32d20cd539c012bd9520e0
KraiyosW <kraiyos.wanna@gmail.com>
2023-03-08 22:49:24 +0700

    fix: picture not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './resume.css'
-import { ReactDOM } from 'react'
 import React from 'react'
-import "./component/landingpage"
 import Header,{Introduction} from './component/landingpage'
 import ContentDetails from './component/content'
 import EducationProfile from './component/education'
